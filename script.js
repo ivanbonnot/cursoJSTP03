1bc03ce9ce54ca75e899fc17968b16a8c6cca15e
Ivan Bonnot <ivanbonnot@gmail.com>
2022-07-25 17:00:14 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const precioProductoString = document.getElementById("precioProducto")
+const elegirProducto = document.getElementById("elegirProducto").value
 const calcular = document.getElementById('calcular')
 const reiniciar = document.getElementById('reinicio')
 const mostrarCalculo = document.getElementById('calculo')
 
 reiniciar.disabled = true
+let productosLista = []
+console.log(elegirProducto)
 
 calcular.addEventListener('click', () => validarInput())
 reiniciar.addEventListener('click', () => LimpiarHTML())
+
 
 //Validar input
 function validarInput() {
@@ -20,19 +24,18 @@ function validarInput() {
     }
 }
 
-
-//Precio sin iva
+//Calculo de precios
 function calculo(precio) {
     const sinIva = precio - (precio * 0.21)
     const seisCuotas = Math.ceil((precio / 6))
     const doceCuotas = Math.ceil((precio + (precio * 0.3)) / 12)
     const totalEnDoce = doceCuotas * 12
 
-    agregarAlHTML(sinIva, seisCuotas, doceCuotas, totalEnDoce)
+    agregarAlHTML(precio, sinIva, seisCuotas, doceCuotas, totalEnDoce)
 }
 
 //Agregar al html
-function agregarAlHTML(sinIva, seisCuotas, doceCuotas, totalEnDoce) {
+function agregarAlHTML(precio, sinIva, seisCuotas, doceCuotas, totalEnDoce) {
     const parrafo = document.createElement('div');
     parrafo.innerHTML = `<p>El precio sin IVA es: $${sinIva}</p> 
                          <p>El precio en 6 cuotas es: $${seisCuotas}. El total es el mismo </p>
@@ -40,8 +43,25 @@ function agregarAlHTML(sinIva, seisCuotas, doceCuotas, totalEnDoce) {
     mostrarCalculo.appendChild(parrafo);
     calcular.disabled = true
     reiniciar.disabled = false
+    agregarAlArray(precio)
 }
 
+//Agregar al Array
+function agregarAlArray(precioProducto) {
+
+    const nombreProducto = document.getElementById('nombreProducto').value
+
+    const productoObj = {
+        id: Date.now(),
+        producto: nombreProducto,
+        precio: precioProducto
+   }
+
+   productosLista = [...productosLista, productoObj]
+
+   console.log(productosLista)
+   
+}
 
 //Limpiar formulario
 function LimpiarHTML() {
