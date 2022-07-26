d392341c49172296a9b4459468918f8b7f7fa5cd
Ivan Bonnot <ivanbonnot@gmail.com>
2022-07-25 22:40:32 -0300

    falta acomodar html y borrar el prod elegido cuando pongo calcular

--- script.js
+++ script.js
@@ -5,18 +5,19 @@ const reiniciar = document.getElementById('reinicio')
 const mostrarCalculo = document.getElementById('calculo')
 
 reiniciar.disabled = true
+elegirProducto.disabled = true
+
 let productosLista = []
-console.log(elegirProducto)
 
 calcular.addEventListener('click', () => validarInput())
 elegirProducto.addEventListener('change', () => mostrarProductoAnterior())
-reiniciar.addEventListener('click', () => LimpiarHTML())
+elegirProducto.addEventListener('click', () => verificacionUnSoloProducto())
+reiniciar.addEventListener('click', () => limpiarHTML())
 
 
 //Validar input
 function validarInput() {
     const precioProducto = parseInt(precioProductoString.value)
-    console.log(precioProducto, typeof (precioProducto))
 
     if (precioProducto === '' || precioProducto <= 0 || isNaN(precioProducto)) {
         console.log("ingrese un precio válido")
@@ -31,12 +32,13 @@ function calculo(precio) {
     const seisCuotas = Math.ceil((precio / 6))
     const doceCuotas = Math.ceil((precio + (precio * 0.3)) / 12)
     const totalEnDoce = doceCuotas * 12
-
+    
     agregarAlHTML(precio, sinIva, seisCuotas, doceCuotas, totalEnDoce)
 }
 
 //Agregar al html
 function agregarAlHTML(precio, sinIva, seisCuotas, doceCuotas, totalEnDoce) {
+
     const parrafo = document.createElement('div');
     parrafo.innerHTML = `<p>El precio sin IVA es: $${sinIva}</p> 
                          <p>El precio en 6 cuotas es: $${seisCuotas}. El total es el mismo </p>
@@ -44,7 +46,11 @@ function agregarAlHTML(precio, sinIva, seisCuotas, doceCuotas, totalEnDoce) {
     mostrarCalculo.appendChild(parrafo);
     calcular.disabled = true
     reiniciar.disabled = false
+    elegirProducto.disabled = false
+
+   
     agregarAlArray(precio)
+    
 }
 
 //Agregar al Array
@@ -78,33 +84,37 @@ function agregarAlSelect() {
         `;
         elegirProducto.appendChild(elem);
     });
-
-
 }
 
+//Mostrar el producto seleccionado en el select
 function mostrarProductoAnterior() {
-    LimpiarHTML()
+    limpiarHTML()
     const elegirProductoValue = document.getElementById("elegirProducto").value
     console.log(elegirProductoValue)
 
     let mostrarProd = productosLista.filter(prod => prod.producto == `${elegirProductoValue}`)
-    console.log(mostrarProd);
 
     const parrafo = document.createElement('div');
-    parrafo.innerHTML = `<p>Producto: $${}</p> 
-                         <p>El precio en 6 cuotas es: $${seisCuotas}. El total es el mismo </p>
-                         <p>El precio en 12 cuotas con recargo es: $${doceCuotas}. El total es de $${totalEnDoce}</p>`
+    parrafo.innerHTML = `<p>Producto: ${mostrarProd[0].producto}</p> 
+                         <p>El precio es: $${mostrarProd[0].precio}</p>`
     mostrarCalculo.appendChild(parrafo);
 }
 
+function verificacionUnSoloProducto() {
+    if( productosLista.length === 1) {
+        console.log("un solo elem")
+        mostrarProductoAnterior()
+    }
+}
+
 //Limpiar formulario
-function LimpiarHTML() {
+function limpiarHTML() {
     while (mostrarCalculo.firstChild) {
         mostrarCalculo.removeChild(mostrarCalculo.firstChild);
     }
 
     reiniciar.disabled = true
-    precioProductoString.value = ''
     document.getElementById('nombreProducto').value = ''
+    precioProductoString.value = ''
     calcular.disabled = false
 }
